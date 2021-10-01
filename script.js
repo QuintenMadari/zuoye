71ddd4804be56ae11f9e7e00059c26a4d72fb09d
qsrninjin <quinten.madari@hotmail.com>
2021-10-01 21:35:52 +0000

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+new Docute({
+        target: '#docute',
+        title: 'Zuoye',
+        theme: 'dark',
+        darkThemeToggler: true,
+	sourcePath: './CC201',
+	nav: [
+	{title: 'Home', link: '/'},
+	{title: 'About', link: '/README2'}
+	],
+	sidebar: [
+	{title:'Quinten',links: 
+		[
+		{title:'kewen-yi',link:'/quinten/kewen-yi'},
+		{title:'kewen er',link:'/quinten/kewen-er'}
+		]},
+	]
+
+      
+})
